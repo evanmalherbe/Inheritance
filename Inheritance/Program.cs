48487423e47917af2e35e4ac27de04db17690a4a
evanmalherbe <evanmalherbe@gmail.com>
2022-12-16 09:26:23 +0200

    Add project files.

---
+++ Inheritance/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+           Text text = new Text();
+            text.Copy();
+            text.Width = 100;
+            text.AddHyperlink("link-" + text.Width);
+        }
+    }
+}
